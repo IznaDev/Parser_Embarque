065b51a97156f41f6c13aafec73aae133d129767
IznaDev <oussama.boukhrass@gmail.com>
2023-05-02 13:43:19 +0200

    ajout d'un nouveau repertoire et correction du cmake

---
+++ automation_compilator/main .cpp
@@ -0,0 +1,9 @@
+#include "parser.hpp"
+
+using namespace std;
+
+int main()
+{
+    cout<<"ici on fait un code qui transforme un fichier json en un fichier automate compilé à l'aide de la librairie parser"<<endl;
+    return 0;
+}
