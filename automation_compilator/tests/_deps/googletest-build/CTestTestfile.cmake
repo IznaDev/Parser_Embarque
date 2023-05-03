835310e4cfe605f52b52b68406b937842735461f
arthur <arthur-lemaire@bbox.fr>
2023-05-03 16:00:00 +0200

    delete content in automation_compilator

--- automation_compilator/tests/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dopel/Documents/C++/json_validator/tests/_deps/googletest-src
-# Build directory: /home/dopel/Documents/C++/json_validator/tests/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
