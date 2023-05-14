373fa02a297c8e0e245c23b2dffbf445a0cc4ae0
mathias <mathias.tiberghien@gmail.com>
2023-05-14 09:46:29 +0200

    Ajout de documentation

--- automation_code_builder/include/arduino_code_builder.hpp
+++ automation_code_builder/include/arduino_code_builder.hpp
@@ -1,5 +1,16 @@
 #pragma once
 
+/**
+ * @file arduino_code_builder.hpp
+ * @author Mathias Tiberghien (mathias.tiberghien@thalesgroup.com)
+ * @brief Deffines Implementations for Arduino code generation
+ * @version 0.1
+ * @date 2023-05-14
+ * 
+ * @copyright © Thales Services Numériques 2023
+ * 
+ */
+
 #include "code_builder.hpp"
 #include <set>
 #include <algorithm>
@@ -7,14 +18,30 @@
 #include <string>
 #include "expression.hpp"
 
+/**
+ * @brief This class is in charge for codde generation of the main Arduino application.
+ * It also generates dynamically the ArduinoDataContext class, embedding the drivers used by json specification.
+ * The code builder also copy the output folder the source code files needed by the main application (expressions, datacontext, ...)
+ * 
+ */
 class ArduinoCodeBuilder : public ICodeBuilder
 {
 
     public:
+        /**
+         * @brief Process the json object representing the automaton specification and generates code files in the output_directory
+         * 
+         * @param json The json object representing the the automaton specification
+         * @param output_directory The folder path where the code will be generated
+         */
         void build(const json& json, const filesystem::path& output_directory) override;
 };
 
-
+/**
+ * @brief Implements IExpressionVisitor to be able to generate expression instantiation code.
+ * This is a Visitor Design Pattern implementation
+ * 
+ */
 class ArduinoCodebuilderExpressionVisitor: public IExpressionVisitor
 {
     ostream& output;
@@ -24,12 +51,53 @@ class ArduinoCodebuilderExpressionVisitor: public IExpressionVisitor
         void add_arg(int& index, const Expression* expr);
         void add_member_ref(const Expression* expr);
     public:
+        /**
+         * @brief Construct a new Arduino Codebuilder Expression Visitor object
+         * 
+         * @param output The outputs stream where the main code will be written
+         * @param epxr_id An index that will define created variables in a unique way
+         */
         ArduinoCodebuilderExpressionVisitor(ostream& output, const string& epxr_id): output(output), expr_id(epxr_id){}
+        /**
+         * @brief The entry method for the visitor that should call accept method on expr instance
+         * 
+         * @param expr A pointer to a base expression
+         */
         void visit(const Expression* expr) override;
+        /**
+         * @brief Handles code generation for constant expressions
+         * 
+         * @param expr A pointer to a constant expression
+         */
         void visit(const Constant_Expression* expr) override;
+        /**
+         * @brief Handles code generation for refeerence expressions
+         * 
+         * @param expr A pointer to a reference expression
+         */
         void visit(const Reference_Expression* expr) override;
+        /**
+         * @brief Handles code generation for binary operation expressions (e.g +,-,>,==,...)
+         * 
+         * @param expr A pointer to a binary operation expression
+         */
         void visit(const Binary_Operation_Expression* expr) override;
+        /**
+         * @brief Handles code generation for affectation expressions (e.g =,+=,-=)
+         * 
+         * @param expr A pointer to an affectation expression
+         */
         void visit(const Affectation_Expression* expr) override;
+        /**
+         * @brief Handles code generation for unary operation expressions (e.g !)
+         * 
+         * @param expr A pointer to an unary operation expression
+         */
         void visit(const Unary_Operation_Expression* expr) override;
+        /**
+         * @brief Handles code generation for function expressions (e.g min, max,...)
+         * 
+         * @param expr A pointer to a function expression
+         */
         void visit(const Function_Expression* expr) override;
 };
--- automation_code_builder/include/automation.hpp
+++ automation_code_builder/include/automation.hpp
@@ -1,5 +1,16 @@
 #pragma once
 
+/**
+ * @file automation.hpp
+ * @author Mathias Tiberghien (mathias.tiberghien@thalesgroup.com)
+ * @brief Defines generic classes used in automaton code generation
+ * @version 0.1
+ * @date 2023-05-14
+ * 
+ * @copyright © Thales Services Numériques 2023
+ * 
+ */
+
 #include "container.hpp"
 #include "data_context.hpp"
 #include <string.h>
@@ -9,61 +20,193 @@
 #include "expr_functions.hpp"
 #include "expr_affectation.hpp"
 
-
+/**
+ * @brief Defines an automaton device settings with settings that will be applied on setup and inputs/outputs definitions
+ * 
+ */
 class DeviceSettings
 {
+    /**
+     * @brief The device setup settings storing properties as a key value dictionary
+     * 
+     */
     Dictionary<const char*, long> config;
+    /**
+     * @brief The device inputs definitions mapping user defined keys with indexes
+     * 
+     */
     Dictionary<const char*, int> inputs;
+    /**
+     * @brief The device outputs definitions mapping user defined keys with indexes
+     * 
+     */
     Dictionary<const char*,int> outputs;
     public:
         DeviceSettings(){}
+        /**
+         * @brief Get the config object
+         * 
+         * @return a const reference to the device setup settings dictionnary
+         */
         const Dictionary<const char*, long>& get_config() const {return config;}
+        /**
+         * @brief Get the inputs object
+         * 
+         * @return a const reference to the device inputs dictionnary
+         */
         const Dictionary<const char*, int>& get_inputs() const {return inputs;}
+        /**
+         * @brief Get the outputs object
+         * 
+         * @return a const reference to the device outputs dictionnary
+         */
         const Dictionary<const char*,int> get_outputs() const {return outputs;}
+        /**
+         * @brief Retrieve the information if the device should be considered as an inpout
+         * 
+         * @return true if inputs dictionary contains entries
+         * @return false otherwise
+         */
         bool is_input()const {return inputs.size()>0;}
+        /**
+         * @brief Retrieve the information if the device should be considered as an output
+         * 
+         * @return true if outputs dictionary contains entries
+         * @return false otherwise
+         */
         bool is_output()const {return outputs.size()>0;}
+        /**
+         * @brief Add an entry to the device setup settings
+         * 
+         * @param id The key of the setting
+         * @param value The value of the setting
+         */
         void add_config(const char* id, long value){config[id]=value;}
+        /**
+         * @brief Add an input definition to the device inputs definitions
+         * 
+         * @param id The key of the input. The value will be the index in the inputs definitions
+         */
         void add_input(const char* id){inputs[id]=inputs.size();}
+        /**
+         * @brief Add an output definition to the device outputs definitions
+         * 
+         * @param id The key of the output. The value will be the index in the outputs definitions
+         */
         void add_output(const char* id){outputs[id]=outputs.size();}
 };
 
+/**
+ * @brief The device interface in an automaton program
+ * 
+ */
 class IDevice
 {
     protected:
+        /**
+         * @brief Identifier of the device
+         * 
+         */
         const char* id;
+        /**
+         * @brief Type name of the device
+         * 
+         */
         const char* type;
+        /**
+         * @brief The device settings
+         * 
+         */
         DeviceSettings settings;
     public:
         IDevice(const char* id, const char* type, const DeviceSettings& settings):id(id), type(type), settings(settings){}
         virtual ~IDevice(){}
         const char* get_id() const {return id;}
         const char* get_type() const {return type;}
+        /**
+         * @brief Checks if the provided settings contain the required information to use the device
+         * 
+         * @return true if the settings are valid
+         * @return false otherwise
+         */
         virtual bool are_settings_valid() const = 0;
+        /**
+         * @brief Initialises the device at setup time
+         * 
+         * @return true 
+         * @return false 
+         */
         virtual bool setup() = 0;
 };
 
+/**
+ * @brief The input device interface in an automation program
+ * 
+ */
 class IInput: public virtual IDevice
 {
     public:
     IInput(){}
+    /**
+     * @brief Get the value of an input related to its definition key
+     * 
+     * @param value_id the user defined value identifier
+     * @return long 
+     */
     virtual long get_value(const char* value_id) const = 0;
 };
 
+/**
+ * @brief The output device interface in an automation program
+ * 
+ */
 class IOutput: public virtual IDevice
 {
     public:
     IOutput(){}
-    virtual bool set_value(const char* value_i, long value) = 0;
-    virtual bool increase_value(const char* value_i, long value) =0;
-    virtual bool decrease_value(const char* value_i, long value) =0;
+    /**
+     * @brief Set the value related to the value identifier
+     * 
+     * @param value_id The user define value identfier in outputs definitions
+     * @param value The new desired value
+     * @return true If the operation suceed
+     * @return false otherwise
+     */
+    virtual bool set_value(const char* value_id, long value) = 0;
+    /**
+     * @brief Increase the value related to the value identifier by provided value
+     * 
+     * @param value_id The user define value identfier in outputs definitions
+     * @param value The increase value
+     * @return true If the operation suceed
+     * @return false otherwise
+     */
+    virtual bool increase_value(const char* value_id, long value) =0;
+    /**
+     * @brief Decrease the value related to the value identifier by provided value
+     * 
+     * @param value_id The user define value identfier in outputs definitions
+     * @param value The decrease value
+     * @return true If the operation suceed
+     * @return false otherwise
+     */
+    virtual bool decrease_value(const char* value_id, long value) =0;
 };
 
+/**
+ * @brief Interface for device that are Inpout and Output in an automaton program
+ * 
+ */
 class IInputOutput: public virtual IInput, public virtual IOutput
 {
     public:
     IInputOutput(){}
 };
 
+/**
+ * @brief The different device types in an automaton program
+ * 
+ */
 enum Device_Type{INPUT_DEVICE, OUTPUT_DEVICE, INPUTOUTPUT_DEVICE, INVALID_DEVICE};
 class IDeviceFactory
 {
